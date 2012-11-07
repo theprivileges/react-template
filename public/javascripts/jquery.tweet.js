c5ccac78fdce5f9944210d8b406cbce90e2052de
Luiz Lopes <luizlopes@gmail.com>
2012-11-07 10:56:30 -0700

    upgrading to Twitter's RESTAPI1.1

--- public/javascripts/jquery.tweet.js
+++ public/javascripts/jquery.tweet.js
@@ -100,14 +100,13 @@
     }
 
     function build_url() {
-      var proto = ('https:' == document.location.protocol ? 'https:' : 'http:');
       if (s.list) {
-        return proto+"//api.twitter.com/1/"+s.username[0]+"/lists/"+s.list+"/statuses.json?per_page="+s.count+"&callback=?";
+        return 'https://api.twitter.com/1.1/'+s.username[0]+'/lists/'+s.list+'/statuses.json?per_page='+s.count+'&callback=?';
       } else if (s.query == null && s.username.length == 1) {
-        return proto+'//api.twitter.com/1/statuses/user_timeline.json?screen_name='+s.username[0]+'&count='+s.count+'&include_rts=0';
+        return 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='+s.username[0]+'&count='+s.count+'&include_rts=false';
       } else {
         var query = (s.query || 'from:'+s.username.join(' OR from:'));
-        return proto+'//search.twitter.com/search.json?&q='+encodeURIComponent(query)+'&rpp='+s.count+'&callback=?';
+        return 'https://search.twitter.com/search.json?&q='+encodeURIComponent(query)+'&rpp='+s.count+'&callback=?';
       }
     }
 
