dfad3eb0ec55040ec6da529b3d800f03841c2f04
Luiz Lopes <luizlopes@gmail.com>
2015-09-13 17:09:20 -0700

    Updating my personal website for move in WA.
    - Update location.
    - Make some improvements.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
  * @author Luiz Lopes <http://twitter.com/theprivileges>
  */
 
-var express, routes, app; 
+var express, routes, app;
 
 express = require('express');
 routes = require('./routes');
@@ -15,7 +15,7 @@ app = module.exports = express.createServer();
 // Configuration
 
 app.configure(function (){
-  "use strict";
+  'use strict';
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
@@ -25,17 +25,17 @@ app.configure(function (){
 });
 
 app.configure('development', function (){
-  "use strict";
+  'use strict';
   app.use(express.static(__dirname + '/public'));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function (){
-  "use strict";
+  'use strict';
   var oneMonth;
   oneMonth = 2592000;
   app.use(express.static(__dirname + '/public', { maxAge: oneMonth }));
-  app.use(express.errorHandler()); 
+  app.use(express.errorHandler());
 });
 
 // Routes
@@ -43,4 +43,4 @@ app.configure('production', function (){
 app.get('/', routes.index);
 
 app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+console.log('Express server listening on port %d in %s mode', app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-
 /*
  * GET home page.
  */
 
 exports.index = function (req, res){
-  "use strict";
-  res.render('index', { title : 'Luiz Lopes - Web Application Developer' });
+  'use strict';
+
+  var data = {
+    title : 'Luiz Lopes - Web Application Developer',
+    description: 'The website of Luiz Lopes, a web application developer, and athlete; living in Seattle, Washington'
+  };
+
+  res.render('index', data);
 };
