ff88de659e43b3383db73aa38e9cf1198f643c88
Luiz Lopes <luiz.lopes@submittable.com>
2018-12-28 15:39:22 -0700

    Better management of button links
    - spread buttons in an array so we can use forEach to add click listener to each button.
    - remove anonymous function from listener in order to reduce memory.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@
 
 import './styles/index.css';
 
-const resetLinks = (element, text) => {
-    return element.innerText = text;
-};
+const resetLinks = (element, text) => element.innerText = text;
 
 const textCallback = (element, text) => {
     window.setTimeout(resetLinks, 2000, element, text);
     return 'Loading...';
 };
 
-const button = document.querySelector('a.button');
-
-button.addEventListener('click', (event) => {
+const clickCallback = (event) => {
     const target = event.target;
     return textCallback(target, target.innerText);
-});
+}
+
+const clickButton = (element) => element.addEventListener('click', clickCallback);
+
+const buttons = document.querySelectorAll('a.button');
+
+[...buttons].forEach(clickButton);
