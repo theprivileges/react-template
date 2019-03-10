e6c061feb0120b6da4f75bacaf9f0079a0612d6b
Luiz Lopes <luiz@wickeddeveloper.com>
2019-03-10 10:50:54 -0600

    Add eslint-loader
    Make sure we lint js scripts prior to transpiling it with babel

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,14 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'eslint-loader'
+        }
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
