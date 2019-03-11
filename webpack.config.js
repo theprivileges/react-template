a9b757de31f04f8375219f5fd46ac428155b2a65
Luiz Lopes <luiz@wickeddeveloper.com>
2019-03-10 18:10:30 -0600

    Update webpack.config.js
    Starting to make webpack configuration more robust by using the same file for both development and production.

--- webpack.config.js
+++ webpack.config.js
@@ -4,58 +4,68 @@ const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: {
-    main: './src/scripts/index.js'
-  },
-  output: {
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].chunk.js',
-    path: path.resolve(__dirname, 'docs'),
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'eslint-loader'
+module.exports = (webpackEnv) => {
+  const isEnvDevelopment === webpackEnv.development;
+  const isEnvProduction === webpackEnv.production;
+
+  return {
+    mode: isEnvProduction ? 'production' : isEnvDevelopment && 'development',
+    bail: isEnvProduction,
+    devtool: isEnvProduction
+      ? 'source-map' : isEnvDevelopment && 'cheap-module-source-map',
+    entry: {
+      main: './src/scripts/index.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      chunkFilename: '[name].chunk.js',
+      path: path.resolve(__dirname, 'docs'),
+    },
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'eslint-loader'
+          }
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.css$/,
+          use:[
+            'style-loader',
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.jade$/,
+          use: {
+            loader: 'pug-loader'
+          }
         }
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/,
-        use:[
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.jade$/,
-        use: {
-          loader: 'pug-loader'
-        }
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin({}),
-    new CopyPlugin(['CNAME']),
-    new HtmlWebpackPlugin({
-      template: './src/index.jade',
-    }),
-    new ScriptExtHtmlWebpackPlugin({
-      defaultAttribute: 'defer'
-    })
-  ],
-  optimization: {
-    runtimeChunk: 'single',
-  }
-};
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin({}),
+      new CopyPlugin(['CNAME']),
+      new HtmlWebpackPlugin({
+        template: './src/index.jade',
+      }),
+      new ScriptExtHtmlWebpackPlugin({
+        defaultAttribute: 'async'
+      })
+    ],
+    optimization: {
+      minimize: isEnvProduction,
+      runtimeChunk: 'single',
+    }
+  };
+}
