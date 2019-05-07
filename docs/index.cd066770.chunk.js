7c0b8a9e1c6370b59864134036887c9be2afbe44
Luiz Lopes <luiz@wickeddeveloper.com>
2019-05-07 16:50:32 -0600

    New build
    - Index file with meta data.

--- docs/index.cd066770.chunk.js
+++ docs/index.cd066770.chunk.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[0],[function(n,w,o){}],[[0,1]]]);
-//# sourceMappingURL=index.4443ba00.chunk.js.map
+//# sourceMappingURL=index.cd066770.chunk.js.map
